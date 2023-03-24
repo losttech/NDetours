a70f9fe8bdb4e06048f3dbcec1e216328b53d56a
Victor Nova <lost@losttech.software>
2023-03-24 13:03:56 -0700

    v0.0.1

---
+++ src/Detour.cs
@@ -0,0 +1,24 @@
+﻿namespace NDetours;
+
+using System.Runtime.InteropServices;
+
+using static PInvoke.Kernel32;
+
+public static class Detour {
+    const string DLL = "detours.dll";
+
+    [DllImport(DLL, SetLastError = true, CharSet = CharSet.Auto)]
+    public static extern unsafe bool CreateProcessWithDlls(string exe, string commandLine,
+                                                           SECURITY_ATTRIBUTES* processAttributes,
+                                                           SECURITY_ATTRIBUTES* threadAttributes,
+                                                           bool inheritHandles,
+                                                           CreateProcessFlags flags,
+                                                           IntPtr lpEnvironment,
+                                                           string currentDirectory,
+                                                           ref STARTUPINFO startupInfo,
+                                                           out PROCESS_INFORMATION processInfo,
+                                                           Int32 dllsCount,
+                                                           char*[] dlls,
+                                                           IntPtr createProcess
+    );
+}
